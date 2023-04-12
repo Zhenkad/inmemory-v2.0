3be71dd2e0f227bcb34f909bcfe114dd20f765a9
zhenkad <evgenii200127@gmail.com>
2023-04-13 09:59:41 +1000

    new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO inmemory (id, description, created, updated, completed) VALUES ('6550a71c-4da2-454c-984f-49a1dd415b91', 'todo1', '2023-04-05 11:11', '2023-04-05 11:12', false)
+INSERT INTO inmemory (id, description, created, updated, completed) VALUES ('6550a71c-4da2-454c-984f-49a1dd415b12', 'todo1', '2023-04-05 11:11', '2023-04-05 11:12', false)
 INSERT INTO inmemory (id, description, created, updated, completed) VALUES ('6550a71c-4da2-454c-984f-49a1dd415b92', 'todo2', '2023-04-05 11:13', '2023-04-05 11:14', false)
 INSERT INTO inmemory (id, description, created, updated, completed) VALUES ('6550a71c-4da2-454c-984f-49a1dd415b93', 'todo3', '2023-04-05 11:15', '2023-04-05 11:15', false)
