36dfa6fd33f8c5ba2b38cf807193d72fdef415df
zhenkad <evgenii200127@gmail.com>
2023-05-29 16:30:27 +1000

    Добавлена структура таблицы пользователей

---
+++ src/main/java/com/example/inmemory/entity/Users.java
@@ -0,0 +1,25 @@
+package com.example.inmemory.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "Users")
+public class Users {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @NotNull
+    private String userName;
+    private String passwd;
+}
